380a5ef91bfb64280a0022b6bdd0e5b228a1e737
alwry <144924749+alwry@users.noreply.github.com>
2025-02-06 03:36:51 -0500

    FETCHED NAMES
    Finally figured out how to fetch names

--- js/main.js
+++ js/main.js
@@ -1,36 +1,44 @@
-// (()=>{
-//     console.log('iife fired')
+(()=>{
+    console.log('iife fired')
 
-//     const names = document.querySelectorAll('.name')
-//     const infoBox = document.querySelector('.info-div')
-//     const title = document.querySelector('.movie-title')
-//     const baseUrl = "https://swapi.dev/api/";
 
-//     function getMovies() {
-//         fetch(${baseUrl}?q=terminator)
-//         .then(response => response.json())
-//         .then(function(response) {
-//             console.log(response);
-//             const movies = response.description;
-//             const ul = document.createElement("ul");
-//             movies.forEach(movie => {
-//                 const li = document.createElement("li");
-//                 const a = document.createElement("a");
-//                 a.textContent = movie["#TITLE"];
-//                 a.dataset.review = movie["#IMDB_ID"];
-//                 li.appendChild(a);
-//                 ul.appendChild(li);
-//             })
-//             movieBox.appendChild(ul);
-//         })
-//         .then(function() {
-//             const links = document.querySelectorAll("#movie-box li a");
-//             links.forEach(function(link){
-//                 link.addEventListener("click", getReview)
-//             })
-//         })
-//         .catch(function(err) {
-//             console.log(err);
-//         })
-//     }
-// })();
+    const title = document.querySelector('.movie-title')
+    const baseUrl = 'https://swapi.dev/api/people/'
+
+    
+
+    function getNames() {
+        fetch(`${baseUrl}?=people`)
+        .then(
+            response => response.json())
+        .then(function(response) {
+            console.log(response);
+            
+            const names = response.results;
+            const ul = document.createElement("ul");
+            ul.classList.add('names-ul')
+            names.forEach(name => {
+                const li = document.createElement("li");
+                const a = document.createElement("a");
+                a.textContent = name["name"];
+                a.dataset.people = name["name"];
+                li.appendChild(a);
+                ul.appendChild(li);
+            })
+
+            const namesBox = document.querySelector('.list-div')
+            namesBox.appendChild(ul);
+        })
+        // .then(function() {
+        //     const links = document.querySelectorAll("#movie-box li a");
+        //     links.forEach(function(link){
+        //         link.addEventListener("click", getReview)
+        //     })
+        // })
+        .catch(function(err) {
+            console.log(err);
+        })
+    }
+
+    getNames()
+})();
