8e41f63478c5247ea423d4fe5a2e1c291a004ba2
alwry <144924749+alwry@users.noreply.github.com>
2025-02-06 22:22:43 -0500

    FETCHED TITLES
    finally fetched titles, opening crawl and release date respectfully for each name

--- js/main.js
+++ js/main.js
@@ -2,13 +2,14 @@
     console.log('iife fired')
 
 
+
     const title = document.querySelector('.movie-title')
-    const baseUrl = 'https://swapi.dev/api/people/'
+    const baseUrl = 'https://swapi.dev/api/'
 
     
 
     function getNames() {
-        fetch(`${baseUrl}?=people`)
+        fetch(`${baseUrl}people`)
         .then(
             response => response.json())
         .then(function(response) {
@@ -21,7 +22,7 @@
                 const li = document.createElement("li");
                 const a = document.createElement("a");
                 a.textContent = name["name"];
-                a.dataset.people = name["name"];
+                a.dataset.films = name["films"].join(' ');
                 li.appendChild(a);
                 ul.appendChild(li);
             })
@@ -29,16 +30,58 @@
             const namesBox = document.querySelector('.list-div')
             namesBox.appendChild(ul);
         })
-        // .then(function() {
-        //     const links = document.querySelectorAll("#movie-box li a");
-        //     links.forEach(function(link){
-        //         link.addEventListener("click", getReview)
-        //     })
-        // })
+
+        .then(function() {
+            const links = document.querySelectorAll(".list-div li a");
+            links.forEach(link => {
+                link.addEventListener("click", getInfo)
+            });
+        })
         .catch(function(err) {
             console.log(err);
         })
     }
 
     getNames()
+
+    function getInfo(e) {
+        e.preventDefault();
+        console.log("Review called")
+        console.log(e.currentTarget.dataset.films);
+        const films = e.currentTarget.dataset.films.split(' ');
+        const infoBox = document.querySelector('.info-div');
+        infoBox.innerHTML = "";
+        console.log(infoBox);
+        
+
+        films.forEach(film => {
+            fetch(film)
+            .then(response => response.json())
+            .then(function(response) {
+                console.log(response.title);
+                console.log(response.release_date);
+                const heading = document.createElement('h2');
+                const span = document.createElement('span');
+                const preg = document.createElement('p')
+                preg.classList.add('p-reg');
+                preg.textContent = response.opening_crawl;
+                span.classList.add('year');
+                span.textContent = response.release_date;
+                heading.classList.add('movie-title');
+                heading.textContent = response.title;
+
+                heading.appendChild(span);
+                infoBox.appendChild(heading);
+                infoBox.appendChild(preg);
+                // const clone = reviewTemplate.content.cloneNode(true);
+                // const reviewDescription = clone.querySelector(".review-description");
+                // reviewDescription.textContent = response.short.review.reviewBody;
+                // reviewCon.appendChild(clone);
+            })
+            .catch()
+        })
+
+    }
+
+
 })();
